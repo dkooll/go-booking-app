008360fe386268357af4b1bf89a08e39bbbb5e16
dkool <dennis.kool@cloudnation.nl>
2023-01-17 10:53:01 +0100

    add user input validation

--- main.go
+++ main.go
@@ -33,24 +33,39 @@ func main() {
 		fmt.Println("Enter the number of tickets you want to book")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicket := userTickets > 0 && userTickets <= remainingTickets
 
-		bookings = append(bookings, firstName+" "+lastName)
+		if isValidName && isValidEmail && isValidTicket {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		fmt.Printf("Thanks you %v %v for booking %v tickets. We have sent a confirmation email to %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("We have %v tickets remaining\n", remainingTickets)
+			fmt.Printf("Thanks you %v %v for booking %v tickets. We have sent a confirmation email to %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("We have %v tickets remaining\n", remainingTickets)
 
-		var firstNames []string
-		for _, x := range bookings {
-			names := strings.Fields(x) // Split the string into a slice of strings
-			firstNames = append(firstNames, names[0])
-		}
+			var firstNames []string
+			for _, x := range bookings {
+				names := strings.Fields(x) // Split the string into a slice of strings
+				firstNames = append(firstNames, names[0])
+			}
 
-		fmt.Printf("The first names of bookings are %v\n", firstNames)
+			fmt.Printf("The first names of bookings are %v\n", firstNames)
 
-		if remainingTickets == 0 {
-			fmt.Println("Sorry we are sold out")
-			break
+			if remainingTickets == 0 {
+				fmt.Println("Sorry we are sold out")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("First or last name is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("Email is invalid")
+			}
+			if !isValidTicket {
+				fmt.Println("You have entered an invalid number of tickets")
+			}
 		}
 	}
 }
